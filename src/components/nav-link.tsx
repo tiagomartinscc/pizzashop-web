2b89dade083831f6e293f1a3538dbb08d522c976
Tiago Martins <tiagomartinscc@hotmail.com>
2024-09-16 06:55:44 -0300

    create a app layout with a header

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,14 @@
+import { Link, LinkProps, useLocation } from 'react-router-dom'
+
+export type NavLink = LinkProps
+
+export function NavLink(props: NavLink) {
+  const { pathname } = useLocation()
+  return (
+    <Link
+      data-current={pathname === props.to}
+      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground"
+      {...props}
+    ></Link>
+  )
+}
