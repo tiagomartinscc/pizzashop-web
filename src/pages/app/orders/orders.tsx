04a84b18bc17ecfc6947e67e6523c8b53b4a9a6d
Tiago Martins <tiagomartinscc@hotmail.com>
2024-09-17 07:48:51 -0300

    create paginate component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -15,10 +16,12 @@ export function Orders() {
   return (
     <>
       <Helmet title="Pedidos" />
+
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
           <OrderTableFilters />
+
           <div className="rounded-md border">
             <Table>
               <TableHeader>
@@ -28,7 +31,7 @@ export function Orders() {
                   <TableHead className="w-[180px]">Realizado há</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
                   <TableHead>Cliente</TableHead>
-                  <TableHead className="w-[140px]">Total do Pedido</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
                   <TableHead className="w-[164px]"></TableHead>
                   <TableHead className="w-[132px]"></TableHead>
                 </TableRow>
@@ -40,6 +43,8 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
