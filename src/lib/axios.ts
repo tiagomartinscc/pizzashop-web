4fef6092a2d88cd92206465f7b43b69b1256ed2a
Tiago Martins <tiagomartinscc@hotmail.com>
2024-09-20 08:50:54 -0300

    get profile and get managed restaurant into account header

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,7 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  // envia os coockies de autenticação para o backend,
+  // sem esta informação não é possivel pegar os dados de autenticação.
+  withCredentials: true,
 })
